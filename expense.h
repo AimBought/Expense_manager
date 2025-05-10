5b9377d3666e6d6858d8fdd5138fb55bd4d9f812
AimBought <tacikfilip@gmail.com>
2025-05-10 13:18:45 +0200

    Added Expense class.

---
+++ expense.h
@@ -0,0 +1,19 @@
+#ifndef EXPENSE_H
+#define EXPENSE_H
+
+#include <QString>
+#include <QDate>
+
+class Expense {
+public:
+    Expense(const QString &category, const QString &product, const QString &paymentMethod, double amount, const QDate &date);
+
+    QString toString() const;
+    QString category;
+    QString product;
+    QString paymentMethod;
+    double amount;
+    QDate date;
+};
+
+#endif // EXPENSE_H
