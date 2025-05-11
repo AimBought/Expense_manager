1f3e7a08d15fe2448a31010c132357a8dbc58c38
AimBought <tacikfilip@gmail.com>
2025-05-11 15:16:39 +0200

    Added simple logger for displaying messages.

---
+++ simplelogger.h
@@ -0,0 +1,12 @@
+#ifndef SIMPLELOGGER_H
+#define SIMPLELOGGER_H
+#include<string>
+
+class SimpleLogger
+{
+public:
+    SimpleLogger();
+    void logMessage(std::string message);
+};
+
+#endif // SIMPLELOGGER_H
